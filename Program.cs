4cb2855ffa54c0b3504b708127bdede63d4e1315
Boris Bajtl <holladiewaldfee2@gmail.com>
2020-02-03 14:06:38 +0100

    next

--- Program.cs
+++ Program.cs
@@ -3,26 +3,29 @@ using System.IO;
 using System.Threading;
 using Telegram.Bot;
 using Telegram.Bot.Args;
+using GoogleApi;
 
 namespace MassmannTelegramBot
 {
     class Program
     {
+        static ITelegramBotClient botClient;
+        static readonly string[] s_Scopes;
+
         static void Main(string[] args)
         {
-            static ITelegramBotClient botClient;
-            string[] sApiToken;
+            string[] s_ApiToken;
             try
             {
-                sApiToken = File.ReadAllLines("../../telegrambot.token");
+                s_ApiToken = File.ReadAllLines("../../telegrambot.token");
                 // System.Console.WriteLine(sApiToken[0]);
             }
             catch (InvalidCastException e)
             {
                 System.Console.WriteLine("error error: {0}", e);
-                sApiToken = new string[1];
+                s_ApiToken = new string[1];
             }
-            botClient = new TelegramBotClient(sApiToken[0]);
+            botClient = new TelegramBotClient(s_ApiToken[0]);
             var me = botClient.GetMeAsync().Result;
             System.Console.WriteLine($"Hello, World! I am user {me.Id} and my name is {me.FirstName}.");
         }
